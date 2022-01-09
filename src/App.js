8430c8a39e119115e30ef18bb8dbd0a4a3116461
Jeongbin Kim <jaguar2521@gmail.com>
2022-01-09 12:59:54 +0900

    idon'tknow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import ButtonOne from "./ButtonOne";
-import ButtonTwo from "./ButtonTwo";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => {
-    setCounter((counter) => (counter += 1));
-  };
-
-  useEffect(() => {
-    console.log("YOyo");
-  }, []);
-
-  return (
-    <div>
-      <h1 className={styles.head}>Clicked {counter} times</h1>
-      <ButtonOne onClick={onClick} />
-      <ButtonTwo />
-    </div>
-  );
+  return <input type="text" placeholder="Write To Do"></input>;
 }
 
 export default App;
