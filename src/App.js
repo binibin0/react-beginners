2aea97b406ffa04d2da9ecee304d6384a2efef15
Jeongbin Kim <jaguar2521@gmail.com>
2022-01-09 15:45:29 +0900

    to-do

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <input type="text" placeholder="Write To Do"></input>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(() => event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo !== "") {
+      setToDos((current) => [toDo, ...current]);
+      setToDo("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Your To Do List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write To Do"></input>
+        <button>Add</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
